ad13261c2662917af758cf692e844eed7c837f93
luislealdev <luisrrleal@gmail.com>
2022-06-15 21:58:06 -0500

    change directories of styles

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,6 @@
 export const getGifs = async( category ) => {
 
     const url = `https://api.giphy.com/v1/gifs/search?api_key=JkQjY2gIMjJNXZTYq5oxaBoOr7aDZn1Y&q=${ category }&limit=10`;
-    // const url = `https://api.giphy.com/v1/gifs/search?q=${category.trim()}&limit=10&api_key=JkQjY2gIMjJNXZTYq5oxaBoOr7aDZn1Y`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
